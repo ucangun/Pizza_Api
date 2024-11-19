e359d446033c3b764ad90dab773ef4b3b51ac384
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-19 11:02:24 +0100

    "Updated token.js route to include controller functions and define routes for CRUD operations"

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,9 +1,21 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 
+const {
+  list,
+  create,
+  read,
+  update,
+  deleteToken,
+} = require("../controllers/token");
+
+router.route("/").get(list).post(create);
+
+router.route("/:id").get(read).put(update).patch(update).delete(deleteToken);
+
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
