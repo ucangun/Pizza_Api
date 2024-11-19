babd0c59ce2cf059bcbe96b489a77d98d72169d0
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-19 09:45:04 +0100

    "Added user schema and model to user.js file"

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,47 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+const { Schema, model } = require("mongoose");
+
 /* ------------------------------------------------------- */
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: (password) => passwordEncrypt(password),
+      //   validate: (password) => true,  ==> in Controller
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Please fill a valid email address",
+      ],
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: "true", collection: "users" }
+);
+
+module.exports = model("User", userSchema);
