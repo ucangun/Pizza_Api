79424a6d154bceb268015c84d11d93924273697a
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-19 11:18:47 +0100

    "Added login functionality to auth controller with password encryption and token generation"

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,45 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+module.exports = {
+  login: async (req, res) => {
+    const { userName, password, email } = req.body;
+
+    if (!(email || userName) && password) {
+      res.errorStatusCode = 401;
+      throw new Error("UserName/Email and Password required!");
+    }
+
+    const user = await User.findOne({ $or: [{ userName }, { email }] });
+
+    if (user?.password !== passwordEncrypt(password)) {
+      res.errorStatusCode = 401;
+      throw new Error("Invalid Credentials!");
+    }
+
+    if (!user.isActive) {
+      res.errorStatusCode = 401;
+      throw new Error("Your account is not active!");
+    }
+
+    let tokenData = await Token.findOne({ userId: user._id });
+
+    if (!tokenData) {
+      tokenData = await Token.create({
+        userId: user._id,
+        token: passwordEncrypt(user._id + Date.now()),
+      });
+    }
+    res.status(201).send({
+      error: false,
+      token: tokenData.token,
+      user,
+    });
+  },
+
+  logout: async (req, res) => {},
+};
